67fe398630b783b820ec2d800787e6559d4872a8
Ayushi Mishra <mishrayu1998@gmail.com>
2026-06-24 15:49:37 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0) return new ArrayList<>(){};
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < strs.length; i++) {
+
+        }
+
+        return new ArrayList<>();
+    }
+}
