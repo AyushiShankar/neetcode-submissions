c61840a49b0ba592c060e976ee01967e29563a75
Ayushi Mishra <mishrayu1998@gmail.com>
2026-06-26 23:05:05 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == " ")
+            return false;
+          
+        int left = 0;
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) == str.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
