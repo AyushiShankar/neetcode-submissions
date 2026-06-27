a021f21ef1138c8f7a86c49449c66e7235c51df2
Ayushi Mishra <mishrayu1998@gmail.com>
2026-06-28 01:24:17 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] height) {
+        int sum = 0, left = 0;
+        int right = 1;
+        while (left < right) {
+            if (height[left] == 0) {
+                left++;
+                right++;
+            } else if (height[right] == 0)
+                right++;
+            else if (right - left == 1) {
+                right++;
+            } else {
+                int area = Math.min(height[left], height[right]) * (right - left);
+                sum += area;
+                left = right;
+                right++;
+            }
+        }
+    }
+    return sum;
+}
+}
