f2cae0dd5e9e31a69f82db0ce332fe00ec7b549f
Ayushi Mishra <mishrayu1998@gmail.com>
2026-07-05 22:07:35 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+
+       for (int row = 0; row < 9; row += 3) {
+           for (int col = 0; col < 9; col += 3) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+
+                char ch = board[i][j];
+
+                if (ch == '.') continue;
+
+                if (set.contains(ch))
+                    return false;
+
+                set.add(ch);
+            }
+        }
+    }
+     }
+     return true;
+}
+}
