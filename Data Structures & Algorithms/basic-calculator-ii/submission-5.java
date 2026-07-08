bf30fd528d1bdef5d1aa9a97513f6e86b45be459
Ayushi Mishra <mishrayu1998@gmail.com>
2026-07-08 13:24:43 +0530

    Add: basic-calculator-ii - submission-5

---
+++ Data Structures & Algorithms/basic-calculator-ii/submission-5.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int calculate(String s) {
+        char[] arr = s.toCharArray();
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        char sign = '+';
+        for(int i = 0 ; i < arr.length ; i++){
+            char ch = arr[i];
+            if(Character.isDigit(ch)){
+                result = result * 10 + (ch - '0');
+            }
+
+            if ((!Character.isDigit(ch) && ch != ' ') || i == arr.length - 1) {
+
+                if (sign == '+') {
+    stack.push(result);
+} else if (sign == '-') {
+    stack.push(-result);
+} else if (sign == '*') {
+    stack.push(stack.pop() * result);
+} else if (sign == '/') {
+    stack.push(stack.pop() / result);
+}
+                 result = 0;
+             sign =ch;
+            }
+            
+        }
+         int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+
+        return ans;
+    }
+}
